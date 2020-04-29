ab7c759316cc2e6939b4185d38635827ff44f3fe
root <chebbi.hessine@gmail.com>
2020-04-29 04:05:31 +0100

    V2 Navbar

--- dockerapi.js
+++ dockerapi.js
@@ -1,17 +1,17 @@
-let Docker = require("dockerode");
-let isWindows = process.platform === "win32";
+const Docker = require("dockerode");
+const isWindows = process.platform === "win32";
 
 let options = {};
 
 if (isWindows) {
-    options = {
-        host: '127.0.0.1',
-        port: 2375
-    }
+  options = {
+    host: "127.0.0.1",
+    port: 2375
+  };
 } else {
-    options = {
-        socketPath: '/var/run/docker.sock'
-    }
+  options = {
+    socketPath: "/var/run/docker.sock"
+  };
 }
 
 module.exports = new Docker(options);
